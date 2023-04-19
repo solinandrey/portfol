b4019da5c3deecf15ace31c70a1ebc8e7526a631
Andrey Solin <a.solin@warpa.ru>
2023-04-19 18:46:47 +0400

    wrong import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Layout from '../components/main-layout/layout';
-import MainPage from '../components/main/MainPage';
-import type { NextPageWithLayout } from './_app'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Layout from "../components/main-layout/layout";
+import MainPage from "../components/main/MainPage";
 
 export default function Home() {
   return (
     <Layout>
-        <MainPage />
+      <MainPage />
     </Layout>
-  )
+  );
 }
